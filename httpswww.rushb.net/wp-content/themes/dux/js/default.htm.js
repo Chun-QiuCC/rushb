da8164bbeeb79b06a81bfadf813d67b2c22a7ad9
chun_qiu <herodain@qq.com>
2023-12-10 00:02:59 +0800

    上传RUSHB中文网镜像站所有内容

---
+++ httpswww.rushb.net/wp-content/themes/dux/js/default.htm.js
@@ -0,0 +1,169 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="dns-prefetch" href="https://apps.bdimg.com/">
+<meta http-equiv="X-UA-Compatible" content="IE=11,IE=10,IE=9,IE=8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">
+<meta name="apple-mobile-web-app-title" content="CSGO RushB中文网">
+<meta http-equiv="Cache-Control" content="no-siteapp">
+<title>CSGO免费开箱模拟器-CSGO RushB中文网</title>
+<link rel='dns-prefetch' href='https://apps.bdimg.com/' />
+<link rel='dns-prefetch' href='default.htm' />
+<link rel='dns-prefetch' href='https://s.w.org/' />
+<link rel='stylesheet' id='_bootstrap-css'  href='wp-content/themes/dux/css/bootstrap.min.css@ver=5.1' type='text/css' media='all' />
+<link rel='stylesheet' id='_fontawesome-css'  href='wp-content/themes/dux/css/font-awesome.min.css@ver=5.1' type='text/css' media='all' />
+<link rel='stylesheet' id='_main-css'  href='wp-content/themes/dux/css/main.css@ver=5.1' type='text/css' media='all' />
+<script type='text/javascript' src='https://apps.bdimg.com/libs/jquery/1.9.1/jquery.min.js?ver=5.1'></script>
+<link rel='https://api.w.org/' href='wp-json/default.htm' />
+<link rel='prev' title='CSGO贴纸推荐：Feral Predators（野性掠食者）' href='csgo-stickers-feral-predators.html/default.htm' />
+<link rel='next' title='V社不再提供2017年11月前CSGO竞技模式记录' href='csgo-match-history-competitive.html/default.htm' />
+<link rel="canonical" href="csgo-open-case-simulator.html/default.htm" />
+<link rel='shortlink' href='csgo-open-case-simulator.html/default.htm' />
+<link rel="alternate" type="application/json+oembed" href="wp-json/oembed/1.0/embedurlhttps_253a_252f_252fwww.rushb.net_252fcsgo-op~1.html" />
+<link rel="alternate" type="text/xml+oembed" href="wp-json/oembed/1.0/embedurlhttps_253a_252f_252fwww.rushb.net_252fcsgo-op~1.xml" />
+<meta name="keywords" content="csgo, 体验, 免费, 开箱模拟器, 概率, 武器箱, 纪念包, 胶囊, CSGO教程攻略">
+<meta name="description" content="为广大非洲玩家推荐免费的CSGO开箱模拟器：convars，不用花一分钱就能体验原汁原味的CSGO所有开箱，当然开出的饰品也无法占为己有。    CSGO开箱模拟器convars为玩家提供了所有武器箱、纪念包以及贴纸胶囊（签名/战队除外），采用了CSGO官方概率，并且还原了游戏中的开箱动画和音效体验。还能记录玩家一共开了多少箱子和皮肤及总价值，以让玩家深刻了解开出好皮肤的概率是有多低。  CSGO">
+<style>a:hover, .site-navbar li:hover > a, .site-navbar li.active a:hover, .site-navbar a:hover, .search-on .site-navbar li.navto-search a, .topbar a:hover, .site-nav li.current-menu-item > a, .site-nav li.current-menu-parent > a, .site-search-form a:hover, .branding-primary .btn:hover, .title .more a:hover, .excerpt h2 a:hover, .excerpt .meta a:hover, .excerpt-minic h2 a:hover, .excerpt-minic .meta a:hover, .article-content .wp-caption:hover .wp-caption-text, .article-content a, .article-nav a:hover, .relates a:hover, .widget_links li a:hover, .widget_categories li a:hover, .widget_ui_comments strong, .widget_ui_posts li a:hover .text, .widget_ui_posts .nopic .text:hover , .widget_meta ul a:hover, .tagcloud a:hover, .textwidget a, .textwidget a:hover, .sign h3, #navs .item li a, .url, .url:hover, .excerpt h2 a:hover span, .widget_ui_posts a:hover .text span, .widget-navcontent .item-01 li a:hover span, .excerpt-minic h2 a:hover span, .relates a:hover span{color: #555555;}.btn-primary, .label-primary, .branding-primary, .post-copyright:hover, .article-tags a, .pagination ul > .active > a, .pagination ul > .active > span, .pagenav .current, .widget_ui_tags .items a:hover, .sign .close-link, .pagemenu li.active a, .pageheader, .resetpasssteps li.active, #navs h2, #navs nav, .btn-primary:hover, .btn-primary:focus, .btn-primary:active, .btn-primary.active, .open > .dropdown-toggle.btn-primary, .tag-clouds a:hover{background-color: #555555;}.btn-primary, .search-input:focus, #bdcs .bdcs-search-form-input:focus, #submit, .plinks ul li a:hover,.btn-primary:hover, .btn-primary:focus, .btn-primary:active, .btn-primary.active, .open > .dropdown-toggle.btn-primary{border-color: #555555;}.search-btn, .label-primary, #bdcs .bdcs-search-form-submit, #submit, .excerpt .cat{background-color: #555555;}.excerpt .cat i{border-left-color:#555555;}@media (max-width: 720px) {.site-navbar li.active a, .site-navbar li.active a:hover, .m-nav-show .m-icon-nav{color: #555555;}}@media (max-width: 480px) {.pagination ul > li.next-page a{background-color:#555555;}}.post-actions .action.action-like{background-color: #555555;}.catleader h1{border-left-color: #555555;}pre {
+white-space: pre-wrap;
+white-space: -moz-pre-wrap;
+white-space: -pre-wrap;
+white-space: -o-pre-wrap;
+word-wrap: break-word;
+}
+.article-content a:hover span{color: #45B6F7;}</style><link rel="shortcut icon" href="favicon.ico">
+<!--[if lt IE 9]><script src="wp-content/themes/dux/js/libs/html5.min.js"></script><![endif]-->
+</head>
+<body class="post-template-default single single-post postid-1050 single-format-standard m-excerpt-cat site-layout-2 text-justify-on">
+<header class="header">
+	<div class="container">
+		<div class="logo"><a href="default.htm" title="RushB中文网-反恐精英全球攻势中文网站-CSGO中文网"><img src="wp-content/uploads/2018/10/rushblogo.png" alt="RushB中文网-反恐精英全球攻势中文网站-CSGO中文网">CSGO RushB中文网</a></div>		<div class="brand">RushB中文网
+<br>CSGO / 反恐精英：全球攻势</div>		<ul class="site-nav site-navbar">
+			<li id="menu-item-15" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-15"><a title="CSGO新闻资讯" href="csgo-news">新闻资讯</a></li>
+<li id="menu-item-3886" class="menu-item menu-item-type-taxonomy menu-item-object-category current-post-ancestor current-menu-parent current-post-parent menu-item-has-children menu-item-3886"><a title="CSGO教程攻略" href="csgo-course">教程攻略</a>
+<ul class="sub-menu">
+	<li id="menu-item-4542" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-4542"><a title="CSGO指令命令" href="csgo-course/csgo-console-commands">CSGO指令命令</a></li>
+</ul>
+</li>
+<li id="menu-item-51" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-51"><a title="CSGO比赛信息" href="csgo-matchs">比赛信息</a></li>
+<li id="menu-item-16" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-16"><a title="CSGO更新日志" href="csgo-updates-log">更新日志</a></li>
+<li id="menu-item-52" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-52"><a title="CSGO资源壁纸" href="csgo-resources-wallpapers">资源壁纸</a></li>
+<li id="menu-item-55" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-55"><a title="CSGO创意工坊" href="csgo-workshop">创意工坊</a></li>
+<li id="menu-item-433" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-433"><a title="CSCO经典攻势" href="csco">经典攻势</a></li>
+<li id="menu-item-5883" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-5883"><a title="CSGO职业选手外设、设置指令" href="pro-player-settings">选手设置</a></li>
+							<li class="navto-search"><a href="javascript:;" class="search-show active"><i class="fa fa-search"></i></a></li>
+					</ul>
+				<div class="topbar">
+			<ul class="site-nav topmenu">
+											</ul>
+					</div>
+						<i class="fa fa-bars m-icon-nav"></i>
+			</div>
+</header>
+<div class="site-search">
+	<div class="container">
+		<form method="get" class="site-search-form" action="default.htm" ><input class="search-input" name="s" type="text" placeholder="输入关键字" value=""><button class="search-btn" type="submit"><i class="fa fa-search"></i></button></form>	</div>
+</div>	<div class="breadcrumbs">
+		<div class="container">当前位置：<a href="default.htm">CSGO RushB中文网</a> <small>></small> <a href="csgo-course">CSGO教程攻略</a> <small>></small> 正文</div>
+	</div>
+<section class="container">
+	<div class="content-wrap">
+	<div class="content">
+				<header class="article-header">
+			<h1 class="article-title"><a href="csgo-open-case-simulator.html/default.htm">CSGO免费开箱模拟器</a></h1>
+			<div class="article-meta">
+				<span class="item">2019-03-08</span>
+												<span class="item">分类：<a href="csgo-course" rel="category tag">CSGO教程攻略</a></span>
+				<span class="item post-views">阅读(64391)</span>				<span class="item">评论(0)</span>
+				<span class="item"></span>
+			</div>
+		</header>
+				<article class="article-content">
+			
+			
+			<div style="width:100%;">
+			    <a target="_blank" onclick="_hmt.push(['_trackEvent', 'd2topbox', 'click', 'post'])" href=" https://www.rushb.net/go/d2topbox" rel="nofollow"><img src="wp-content/uploads/2022/06/new820150.png"></a>
+            </div>
+			
+			
+						<p>为广大非洲玩家推荐免费的CSGO开箱模拟器：convars，不用花一分钱就能体验原汁原味的CSGO所有开箱，当然开出的饰品也无法占为己有。</p>
+<p><img class="alignnone size-medium wp-image-1054" src="wp-content/uploads/2019/03/convars2-820x456.png" alt="" width="820" height="456" srcset="wp-content/uploads/2019/03/convars2.png 820w, https_3A//www.rushb.net/wp-content/uploads/2019/03/convars2-768x427.png 768w" sizes="(max-width: 820px) 100vw, 820px" /></p>
+<p>CSGO开箱模拟器convars为玩家提供了所有武器箱、纪念包以及贴纸胶囊（签名/战队除外），采用了CSGO官方概率，并且还原了游戏中的开箱动画和音效体验。还能记录玩家一共开了多少箱子和皮肤及总价值，以让玩家深刻了解开出好皮肤的概率是有多低。</p>
+<p><strong>CSGO开箱模拟器convars网址：<a href="https://convars.com/case/en" target="_blank" rel="noopener">https:https://convars.com/case/en</a></strong></p>
+<p><img class="alignnone size-medium wp-image-1053" src="wp-content/uploads/2019/03/convars4-820x471.png" alt="" width="820" height="471" srcset="wp-content/uploads/2019/03/convars4.png 820w, https_3A//www.rushb.net/wp-content/uploads/2019/03/convars4-768x441.png 768w" sizes="(max-width: 820px) 100vw, 820px" /></p>
+<p><img class="alignnone size-medium wp-image-1052" src="wp-content/uploads/2019/03/convars7-820x270.png" alt="" width="820" height="270" srcset="wp-content/uploads/2019/03/convars7.png 820w, https_3A//www.rushb.net/wp-content/uploads/2019/03/convars7-768x253.png 768w" sizes="(max-width: 820px) 100vw, 820px" /></p>
+		</article>
+								            <div class="post-actions">
+            	<a href="javascript:;" etap="like" class="post-like action action-like" data-pid="1050"><i class="fa fa-thumbs-o-up"></i>赞(<span>154</span>)</a>            	            	            </div>
+        		<div class="post-copyright">未经允许不得转载：<a href="default.htm">CSGO RushB中文网</a> &raquo; <a href="csgo-open-case-simulator.html/default.htm">CSGO免费开箱模拟器</a></div>		
+					<div class="action-share"></div>
+		
+		<div class="article-tags">标签：<a href="tag/csgo" rel="tag">csgo</a><a href="tag/_25e4_25bd_2593_25e9_25aa_258c" rel="tag">体验</a><a href="tag/_25e5_2585_258d_25e8_25b4_25b9" rel="tag">免费</a><a href="tag/_25e5_25bc_2580_25e7_25ae_25b1_25e6_25a8_25a1_25e6_258b_259f_25e5_2599_25a8" rel="tag">开箱模拟器</a><a href="tag/_25e6_25a6_2582_25e7_258e_2587" rel="tag">概率</a><a href="tag/_25e6_25ad_25a6_25e5_2599_25a8_25e7_25ae_25b1" rel="tag">武器箱</a><a href="tag/_25e7_25ba_25aa_25e5_25bf_25b5_25e5_258c_2585" rel="tag">纪念包</a><a href="tag/_25e8_2583_25b6_25e5_259b_258a" rel="tag">胶囊</a></div>
+		
+		
+		            <nav class="article-nav">
+                <span class="article-nav-prev">上一篇<br><a href="csgo-stickers-feral-predators.html/default.htm" rel="prev">CSGO贴纸推荐：Feral Predators（野性掠食者）</a></span>
+                <span class="article-nav-next">下一篇<br><a href="csgo-match-history-competitive.html/default.htm" rel="next">V社不再提供2017年11月前CSGO竞技模式记录</a></span>
+            </nav>
+        
+				<div class="relates"><div class="title"><h3>推荐</h3></div><ul><li><a href="csgo-workshop-map-mirage-plus.html/default.htm">创意工坊地图推荐：荒漠迷城+</a></li><li><a href="csgo-case-operationoperation-release-order-time.html/default.htm">CSGO已推出的武器箱、大行动更新时间列表【新增印花胶囊】</a></li><li><a href="csgo-removed-map-chlorine.html/default.htm">被移除的CSGO地图：科洛林</a></li><li><a href="csgo-workshop-map-palais.html/default.htm">CSGO搭档地图推荐：Palais</a></li><li><a href="csgo-update-2022-12-17.html/default.htm">CSGO 2022年12月17日更新日志「多张地图改进」</a></li></ul></div>					</div>
+	</div>
+	<div class="sidebar">
+<div class="widget_text widget widget_custom_html"><div class="textwidget custom-html-widget"><div>
+	<a target="_blank" onclick="_hmt.push(['_trackEvent', 'hash', 'click', 'pc_all_right2'])" href="	https://www.rushb.net/go/hash" rel="nofollow"><img src="wp-content/uploads/2021/07/88hash360.png" alt="88s"></a>
+</div></div></div><div class="widget widget_ui_posts"><h3>置顶推荐</h3><ul><li><a target="_blank" href="csgo-map-agent-lab.html/default.htm"><span class="thumbnail"><img data-src="wp-content/uploads/2021/02/agentlab2021-1-220x150.png" alt="CSGO 探员、刀子、音乐盒 体验地图：Agent Lab-CSGO RushB中文网" src="wp-content/uploads/2021/02/agentlab2021-1-220x150.png" class="thumb"></span><span class="text">CSGO 探员、刀子、音乐盒 体验地图：Agent Lab</span><span class="muted">2021-02-20</span></a></li>
+<li><a target="_blank" href="csgo-all-give-commands.html/default.htm"><span class="thumbnail"><img data-src="wp-content/uploads/2020/03/csgogive-220x150.png" alt="CSGO武器、装备Give获取控制台指令【2022版】-CSGO RushB中文网" src="wp-content/uploads/2020/03/csgogive-220x150.png" class="thumb"></span><span class="text">CSGO武器、装备Give获取控制台指令【2022版】</span><span class="muted">2020-03-21</span></a></li>
+<li><a target="_blank" href="csgo-name-change-font.html/default.htm"><span class="thumbnail"><img data-src="wp-content/uploads/2019/03/namefont-220x150.png" alt="CSGO玩家名称/武器改名卡使用花体、斜体等特殊字体方法-CSGO RushB中文网" src="wp-content/uploads/2019/03/namefont-220x150.png" class="thumb"></span><span class="text">CSGO玩家名称/武器改名卡使用花体、斜体等特殊字体方法</span><span class="muted">2019-09-14</span></a></li>
+</ul></div><div class="widget widget_ui_posts"><h3>热门文章</h3><ul class="nopic"><li><a target="_blank" href="olofmeister-defused-csgo-wallpaper.html/default.htm"><span class="text">Olofmeister/奥拉夫 火中拆包 壁纸</span><span class="muted">2022-08-14</span></a></li>
+<li><a target="_blank" href="csgo-deathnotices-command.html/default.htm"><span class="text">CSGO关闭右上角击杀提示指令</span><span class="muted">2022-01-19</span></a></li>
+<li><a target="_blank" href="csgo-all-give-commands.html/default.htm"><span class="text">CSGO武器、装备Give获取控制台指令【2022版】</span><span class="muted">2020-03-21</span></a></li>
+<li><a target="_blank" href="csgo-map-piranesi.html/default.htm"><span class="text">CSGO地图推荐：Piranesi（皮诺内西城堡）</span><span class="muted">2019-07-04</span></a></li>
+<li><a target="_blank" href="csgo-demo-voice-volume.html/default.htm"><span class="text">CSGO观看DEMO回放关闭/屏蔽玩家语音教程</span><span class="muted">2019-05-13</span></a></li>
+</ul></div></div></section>
+
+
+<footer class="footer">
+	<div class="container">
+							<div class="fcode">
+				<a style="font-size:14px;" target="_blank" rel="nofollow" href="connect">
+<b>网站合作 联系本站</b>
+</a>
+ | 
+<a style="font-size:14px;" target="_blank" rel="nofollow" href="go/bilibili">
+<b>哔哩哔哩</b>
+</a>
+ | 
+<a style="font-size:14px;" target="_blank" rel="nofollow" href="go/qq">
+<b>QQ群 97574671</b>
+</a>
+			</div>
+				<p>&copy; 2023 <a href="default.htm">CSGO RushB中文网</a> &nbsp; <a href="sitemap.xml">网站地图</a>
+</p>
+		<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?f66fd8235142d9ab480f67b9d46e106f";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>	</div>
+</footer>
+
+
+
+<script>
+window.jsui={
+	www: 'default.htm',
+	uri: 'wp-content/themes/dux',
+	ver: '5.1',
+	roll: ["1","2"],
+	ajaxpager: '0',
+	url_rp: 'connect'
+};
+</script>
+<script type='text/javascript' src='https://apps.bdimg.com/libs/bootstrap/3.2.0/js/bootstrap.min.js?ver=5.1'></script>
+<script type='text/javascript' src='wp-content/themes/dux/js/loader.jsver5.1.js'></script>
+<script type='text/javascript' src='wp-includes/js/wp-embed.min.jsver4.9.22.js'></script>
+</body>
+</html>
